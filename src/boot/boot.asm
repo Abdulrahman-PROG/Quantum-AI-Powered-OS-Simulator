91d60ea7e4257eff3c56bd128c49dae960b4aacc
Abdulrahman-PROG <abdoelbanna240@gmail.com>
2025-04-02 04:07:07 +0200

    Add GDT setup to boot.asm

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -17,3 +17,28 @@ start:
     call print_string
 
 boot_msg db 'Quantum OS Booting...', 0
+
+cli                    ; Disable interrupts
+lgdt [gdt_descriptor]  ; Load GDT
+
+gdt_start:
+    dq 0x0             ; Null descriptor
+gdt_code:
+    dw 0xffff         ; Limit
+    dw 0x0            ; Base (low)
+    db 0x0            ; Base (mid)
+    db 10011010b      ; Access byte (code segment)
+    db 11001111b      ; Flags + limit
+    db 0x0            ; Base (high)
+gdt_data:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b      ; Access byte (data segment)
+    db 11001111b
+    db 0x0
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1  ; Size
+    dd gdt_start                ; Address
